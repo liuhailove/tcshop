9dca239d26227fb4ad137fb392c4b777955164f5
honggang.liu <honggang.liu@shopee.com>
2023-12-24 10:31:06 +0800

    init

---
+++ src/main/java/com/shop/tcshop/TcshopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.tcshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/tcshop/TcshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.tcshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
